056d80186243139129ff03dfa7627cacbffc6f71
alex-xavier1 <alex.xavier@iinerds.com>
2025-02-28 22:34:06 +0530

    made changes in db

--- database.py
+++ database.py
@@ -10,24 +10,4 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:admin@local
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
-Database_user
-Database_userid
-db_creds
-datbase
-db
-database_session...
-import datetime
-
-importlib
-import lambda 
-import models
-import module
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()updated client
+ db_user
